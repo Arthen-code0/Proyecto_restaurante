ba05ec0d3bff9002f26a4b894c8f4aab003a81df
José Luis Mármol Gómez <jluismarmolgomez@gmail.com>
2025-04-21 16:41:07 +0200

    no dire nada mas

--- Proyecto/proyecto_restaurante/settings.py
+++ Proyecto/proyecto_restaurante/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,7 @@ import os
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
 ]
 
 # Default primary key field type
