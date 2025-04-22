f1500fa2158d7a03d317ca8e82944b9061fda661
LucDomi <ldominguezsanchez@safareyes.es>
2025-04-22 16:18:44 +0200

    Página de creación de platos
    Página de creación de platos creada, falta modificación para que quede mejor.

--- Proyecto/umamihouseapp/urls.py
+++ Proyecto/umamihouseapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from umamihouseapp.views import pagina_principal, pagina_menu, registrar_usuario, inicio_de_sesion
+from umamihouseapp.views import pagina_principal, pagina_menu, registrar_usuario, inicio_de_sesion, crear_plato
 
 urlpatterns = [
     path('home', pagina_principal, name='home'),
@@ -7,5 +7,5 @@ urlpatterns = [
     path('menu/', pagina_menu, name='menu'),
     path('registro/', registrar_usuario, name='registrar_usuario'),
     path('inicio_de_sesion/', inicio_de_sesion, name='inicio_de_sesion'),
-
+    path('crear_plato/', crear_plato, name='crear_plato'),
 ]
--- Proyecto/umamihouseapp/views.py
+++ Proyecto/umamihouseapp/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 
+
 def pagina_principal(request):
     return render(request, 'pagina_principal.html')
+
 
 def pagina_menu(request):
     return render(request, 'pagina_menu.html')
 
+
 def registrar_usuario(request):
     return render(request, 'registro.html')
 
+
 def inicio_de_sesion(request):
     return render(request, 'inicio_sesion.html')
+
+def crear_plato(request):
+    return render(request, 'crear_plato.html')
