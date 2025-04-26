c512e2e0bb053373b3c03d3a67cf5c2271e3d707
José Luis Mármol Gómez <jluismarmolgomez@gmail.com>
2025-04-26 17:08:39 +0200

    Cambios pagina menú
    Agregación de tarjetas y botones de incorporación al carrito de compra.

--- Proyecto/proyecto_restaurante/settings.py
+++ Proyecto/proyecto_restaurante/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'proyecto_restaurante.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'XE',
+        'USER': 'C##PROYECTO_RESTAURANTE',
+        'PASSWORD': '123456',
+        'HOST': 'LOCALHOST',
+        'PORT': '1521',
     }
 }
 
