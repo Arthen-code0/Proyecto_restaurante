d4c14a9735ded8de60c6ef921c7d30bb7bc6ceee
José Luis Mármol Gómez <jluismarmolgomez@gmail.com>
2025-04-30 16:18:25 +0200

    no c leche

--- Proyecto/umamihouseapp/views.py
+++ Proyecto/umamihouseapp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
 
 def pagina_principal(request):
     return render(request, 'pagina_principal.html')
