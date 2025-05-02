9f05020bf65006bcefaec501e2fc199be1c07795
Rosario <rosariiopochiherrera@gmail.com>
2025-05-02 17:27:17 +0200

    Login y registro
    Agregue al .views el login y registro. nos falta agregar un boton de cerrar sesion para los html. IMPORTANTE

--- Proyecto/umamihouseapp/views.py
+++ Proyecto/umamihouseapp/views.py
@@ -1,4 +1,9 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404, redirect
+
+from umamihouseapp.form import RegistroForm, LoginForm
+
 
 def pagina_principal(request):
     return render(request, 'pagina_principal.html')
@@ -9,12 +14,35 @@ def pagina_menu(request):
 
 
 def registrar_usuario(request):
-    return render(request, 'registro.html')
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit==False)
+            usuario.set_password(form.cleaned_data['password'])
+            usuario.save()
+            return redirect('login')
+        else:
+            form = RegistroForm()
+            return render(request, 'registro.html', {'form': form})
 
 
 def inicio_de_sesion(request):
-    return render(request, 'inicio_sesion.html')
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            usuario = authenticate(request, email=email, password=password)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('login')
+            else:
+                form = LoginForm()
+                return render(request, 'registro.html', {'form': form})
 
+def logout_usuario(request):
+    logout(request)
+    return redirect('login')
 
 def crear_plato(request):
     return render(request, 'crear_plato.html')
@@ -30,6 +58,7 @@ def tu_pedido(request):
 
 def camarero(request):
     return render(request, 'camarero.html')
+
 
 def cocinero(request):
     return render(request, 'cocinero.html')
